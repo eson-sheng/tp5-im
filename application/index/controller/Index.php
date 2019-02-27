b3aa4fbe34bcd8cc30dab7b5156f01252484e226
eson <834767372@qq.com>
2019-02-27 23:43:12 +0800

    【更新】- 测试创建

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,8 +1,6 @@
 <?php
 namespace app\index\controller;
 
-use think\Db;
-
 class Index
 {
     public function index()
@@ -10,22 +8,4 @@ class Index
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
     }
 
-    public function test()
-    {
-        /*测试数据库*/
-        $ret = Db::name('test')->find();
-        dump($ret);
-
-        /*测试配置文件*/
-        dump(\think\Config::get());
-
-        /*测试网易云信api*/
-        $IMApiModel = new \app\api\model\IMApi();
-        $info = $IMApiModel->getUinfoss(['10001']);
-        dump($info);
-
-        /*测试redis服务*/
-        $redis = new \think\Cache\driver\Redis();
-        dump($redis->handler->ping());
-    }
 }
---
+++ application/test/controller/Index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eson
+ * Date: 2019-02-27
+ * Time: 23:40
+ */
+
+namespace app\test\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        /*测试数据库*/
+        $ret = \think\Db::name('test')->find();
+        dump($ret);
+
+        /*测试配置文件*/
+        dump(\think\Config::get()['im_appkey']);
+
+        /*测试网易云信api*/
+        $IMApiModel = new \app\api\model\IMApi();
+        $info = $IMApiModel->getUinfoss(['10001']);
+        dump($info);
+
+        /*测试redis服务*/
+        $redis = new \think\Cache\driver\Redis();
+        dump($redis->handler->ping());
+    }
+}
