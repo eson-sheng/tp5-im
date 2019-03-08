0b6db689a02b1293916f65651a97d065b2a3d67b
eson <834767372@qq.com>
2019-03-08 14:49:58 +0800

    【bug】- 解决验证码跨域问题

--- application/api/lib/VerifyImage.php
+++ application/api/lib/VerifyImage.php
@@ -64,6 +64,11 @@ class VerifyImage
             imageline($image, rand(0, $width), rand(0, $height), rand(0, $width), rand(0, $height), $color);
         }
         //输出图片
+        $client_host = \think\Config::get()['client_host'];
+        header("Access-Control-Allow-Origin:{$client_host}");// 允许设置的来源访问
+        header("Access-Control-Allow-Credentials:true");// 允许携带cookie
+        header("Access-Control-Allow-Method:POST,GET");// 允许访问的方式
+        header("Access-Control-Allow-Headers:content-type");// 允许的类型，默认只支持三个
         header("Content-type: image/png");
         imagepng($image);
         //释放资源
